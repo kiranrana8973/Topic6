9cf1dc36c1521683a62d3e551b2b0cc4a12a0c8a
kiran rana <kiranrana8973@gmail.com>
2019-04-20 23:45:59 +0545

    Added fragments in activity

---
+++ app/src/main/java/com/example/topic6/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.topic6;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import fragments.FirstFragment;
+import fragments.SecondFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnFragment;
+    //if status is true them load first fragment
+    private Boolean status = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFragment = findViewById(R.id.btnFragment);
+        btnFragment.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (status) {
+            FirstFragment firstFragment = new FirstFragment();
+            //fragmentTransaction.add(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.replace(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load Second Fragment");
+            status = false;
+
+        } else {
+
+            SecondFragment secondFragment = new SecondFragment();
+          //  fragmentTransaction.add(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.replace(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load First Fragment");
+            status = true;
+        }
+    }
+}
